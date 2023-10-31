bf44351b863fe26a3770d169a25e33e47de20b1c
jpaugusto13 <jpaugusto13@proton.me>
2023-10-31 10:55:59 -0300

    adicionando o express

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
-import { create } from "venom-bot";
+import { create } from 'venom-bot';
+import express, { Request, Response } from 'express';
+import startChatbot from './config/startChatBot';
 
-import startChatbot from "./config/startChatBot";
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-create({
-    session: 'graficaArteOf'
+app.get('/', (req: Request, res: Response) => {
+  console.log('Alguém acessou o servidor!');
+  res.send('Servidor na nuvem com console.');
+});
+
+create({ session: 'graficaArteOf' })
+  .then((client) => {
+    app.listen(PORT, () => {
+      console.log(`Servidor rodando na porta ${PORT}`);
+    });
+    startChatbot(client);
   })
-
-  .then((client) => startChatbot(client))
   .catch((erro) => {
     console.log(erro);
   });
