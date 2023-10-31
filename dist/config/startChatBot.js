658609e4f0a338efdfca1e25530f3a96ce921403
jpaugusto13 <jpaugusto13@proton.me>
2023-10-31 13:06:04 -0300

    Sucess2

--- dist/config/startChatBot.js
+++ dist/config/startChatBot.js
@@ -1,42 +1,91 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+
+// src/config/startChatBot.ts
+var startChatBot_exports = {};
+__export(startChatBot_exports, {
+  default: () => startChatBot_default
+});
+module.exports = __toCommonJS(startChatBot_exports);
+
+// src/messages/saudacoes.ts
+var date = /* @__PURE__ */ new Date();
+var hour = date.getHours();
+var saudacao = "Bem-vindo \xE0 Doceria T\xE1ssia Augusto!\nDelivery: *13:00* as *19:00*";
+var day = date.getDay();
+var getGreeting = (hour2) => {
+  if (hour2 >= 13 && hour2 < 18) {
+    return `Ol\xE1, boa tarde! ${saudacao}`;
+  } else if (hour2 >= 18 && hour2 < 19) {
+    return `Ol\xE1, boa noite! ${saudacao}
+
+`;
+  } else if (hour2 >= 19) {
+    return `Ol\xE1, boa noite! ${saudacao}
+
+Nosso delivery encerra as 19:00, no momento estamos fechado, estamos apenas recebendo encomendas de Kit festa!`;
+  } else {
+    return `Ol\xE1, bom dia! ${saudacao}
+
+Estamos em produ\xE7\xE3o, deixe agendado seu pedido!`;
+  }
+};
+var saudacoes = (message, client) => {
+  const greeting = getGreeting(hour);
+  const messageText = `${greeting}`;
+  if (day != 3) {
+    client.sendText(message.from, messageText).then((result) => {
+      console.log("Result: ", result);
+    }).catch((error) => {
+      console.error("Error when sending: ", error);
     });
+  } else {
+    client.sendText(message.from, `${saudacao}
+ Quarta-feira n\xE3o funcionamos!`).then((result) => {
+      console.log("Result: ", result);
+    }).catch((error) => {
+      console.error("Error when sending: ", error);
+    });
+  }
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const saudacoes_1 = __importDefault(require("../messages/saudacoes"));
+var saudacoes_default = saudacoes;
+
+// src/config/startChatBot.ts
 function startChatbot(client) {
-    client.onMessage((message) => __awaiter(this, void 0, void 0, function* () {
-        const text = message.body.toLowerCase();
-        if (!text) {
-            return "";
-        }
-        if (text.includes("endereço") || text.includes("endereco") || text.includes("localização")) {
-            client
-                .sendText(message.from, 'Rua Antônio de Alencar \nN° 1001 \nCoqueiral - Maracanaú')
-                .then((result) => {
-                console.log('Result: ', result);
-            });
-        }
-        // Saudações
-        if (text.includes("bom dia") || text.includes("boa tarde") || text.includes("boa noite") || text.includes("oi") || text.includes("ola") || text.includes("opa") || text.includes("ei tassia")) {
-            (0, saudacoes_1.default)(message, client);
-        }
-        if (text.includes("pix") && message.isGroupMsg === false) {
-            client
-                .sendText(message.from, 'Pix - telefone\n85986681391\nFrancisco Paulo Pereira\n\nMercado Pago\n\nNos envia o comprovante após o pagamento. 😊💙')
-                .then((result) => {
-                console.log('Result: ', result);
-            });
-        }
-    }));
+  client.onMessage(async (message) => {
+    const text = message.body.toLowerCase();
+    if (!text) {
+      return "";
+    }
+    if (text.includes("endere\xE7o") || text.includes("endereco") || text.includes("localiza\xE7\xE3o")) {
+      client.sendText(message.from, "Rua Ant\xF4nio de Alencar \nN\xB0 1001 \nCoqueiral - Maracana\xFA").then((result) => {
+        console.log("Result: ", result);
+      });
+    }
+    if (text.includes("bom dia") || text.includes("boa tarde") || text.includes("boa noite") || text.includes("oi") || text.includes("ola") || text.includes("opa") || text.includes("ei tassia")) {
+      saudacoes_default(message, client);
+    }
+    if (text.includes("pix") && message.isGroupMsg === false) {
+      client.sendText(message.from, "Pix - telefone\n85986681391\nFrancisco Paulo Pereira\n\nMercado Pago\n\nNos envia o comprovante ap\xF3s o pagamento. \u{1F60A}\u{1F499}").then((result) => {
+        console.log("Result: ", result);
+      });
+    }
+  });
 }
-exports.default = startChatbot;
+var startChatBot_default = startChatbot;
--- dist/messages/saudacoes.js
+++ dist/messages/saudacoes.js
@@ -1,43 +1,65 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const date = new Date();
-const hour = date.getHours();
-const saudacao = "Bem-vindo à Doceria Tássia Augusto!\nDelivery: *13:00* as *19:00*";
-const day = date.getDay();
-const getGreeting = (hour) => {
-    if (hour >= 13 && hour < 18) {
-        return `Olá, boa tarde! ${saudacao}`;
-    }
-    else if (hour >= 18 && hour < 19) {
-        return `Olá, boa noite! ${saudacao}\n\n`;
-    }
-    else if (hour >= 19) {
-        return `Olá, boa noite! ${saudacao}\n\nNosso delivery encerra as 19:00, no momento estamos fechado, estamos apenas recebendo encomendas de Kit festa!`;
-    }
-    else {
-        return `Olá, bom dia! ${saudacao}\n\nEstamos em produção, deixe agendado seu pedido!`;
-    }
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
 };
-const saudacoes = (message, client) => {
-    const greeting = getGreeting(hour);
-    const messageText = `${greeting}`;
-    if (day != 3) {
-        client.sendText(message.from, messageText)
-            .then((result) => {
-            console.log('Result: ', result);
-        })
-            .catch((error) => {
-            console.error('Error when sending: ', error);
-        });
-    }
-    else {
-        client.sendText(message.from, `${saudacao}\n Quarta-feira não funcionamos!`)
-            .then((result) => {
-            console.log('Result: ', result);
-        })
-            .catch((error) => {
-            console.error('Error when sending: ', error);
-        });
-    }
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
 };
-exports.default = saudacoes;
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+
+// src/messages/saudacoes.ts
+var saudacoes_exports = {};
+__export(saudacoes_exports, {
+  default: () => saudacoes_default
+});
+module.exports = __toCommonJS(saudacoes_exports);
+var date = /* @__PURE__ */ new Date();
+var hour = date.getHours();
+var saudacao = "Bem-vindo \xE0 Doceria T\xE1ssia Augusto!\nDelivery: *13:00* as *19:00*";
+var day = date.getDay();
+var getGreeting = (hour2) => {
+  if (hour2 >= 13 && hour2 < 18) {
+    return `Ol\xE1, boa tarde! ${saudacao}`;
+  } else if (hour2 >= 18 && hour2 < 19) {
+    return `Ol\xE1, boa noite! ${saudacao}
+
+`;
+  } else if (hour2 >= 19) {
+    return `Ol\xE1, boa noite! ${saudacao}
+
+Nosso delivery encerra as 19:00, no momento estamos fechado, estamos apenas recebendo encomendas de Kit festa!`;
+  } else {
+    return `Ol\xE1, bom dia! ${saudacao}
+
+Estamos em produ\xE7\xE3o, deixe agendado seu pedido!`;
+  }
+};
+var saudacoes = (message, client) => {
+  const greeting = getGreeting(hour);
+  const messageText = `${greeting}`;
+  if (day != 3) {
+    client.sendText(message.from, messageText).then((result) => {
+      console.log("Result: ", result);
+    }).catch((error) => {
+      console.error("Error when sending: ", error);
+    });
+  } else {
+    client.sendText(message.from, `${saudacao}
+ Quarta-feira n\xE3o funcionamos!`).then((result) => {
+      console.log("Result: ", result);
+    }).catch((error) => {
+      console.error("Error when sending: ", error);
+    });
+  }
+};
+var saudacoes_default = saudacoes;
