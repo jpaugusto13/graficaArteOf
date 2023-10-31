5232a4bc13848832eff579f168f0c9d1b8aa5c44
jpaugusto13 <jpaugusto13@proton.me>
2023-10-31 11:39:55 -0300

    servidor express com venom-bot

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,25 @@
-import { create } from "venom-bot";
+import { create } from 'venom-bot';
+import express, { Request, Response } from 'express';
+import startChatbot from './config/startChatBot';
 
-import startChatbot from "./config/startChatBot";
+const app = express();
+const PORT = 8080;
 
-create({
-    session: 'graficaArteOf'
+app.get('/', (req: Request, res: Response) => {
+  console.log('Alguém acessou o servidor!');
+  res.send('Servidor na nuvem com console.');
+});
+
+create({ session: 'graficaArteOf' })
+  .then((client) => {
+    // Inicialize o servidor Express após criar a sessão Venom
+    app.listen(PORT, () => {
+      console.log(`Servidor rodando na porta ${PORT}`);
+    });
+
+    // Implemente a lógica para iniciar o chatbot
+    startChatbot(client);
   })
-
-  .then((client) => startChatbot(client))
   .catch((erro) => {
     console.log(erro);
   });
